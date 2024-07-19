2a726508702ec69051acd961c6ec86a2bfac6c7f
abdullah-337487 <abdullahmustafa337487@gmail.com>
2024-07-19 07:06:22 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            pick();
+        }
+        static void pick()
+        {
+            int[] arr = { 10, 12, 4, 5, 9 };
+            int result1 = 0;
+           
+            for(int i = arr.Length - 1; i >= 0; i--)
+    {
+                for (int j = 0; j < arr.Length; j++)
+                {
+
+                    int indexi = i;
+                    int indexj = j;
+                    if ( indexi == 0 && indexj == 0 || arr[i] == arr[j])
+                    {
+                        result1 = 0;
+                        break;
+                    }
+                    
+                    else
+                    {
+                        result1 = arr[i] - arr[j];
+                    }
+                    
+                    if (result1 > 0)
+                    {
+                        Console.WriteLine(result1);
+                    }
+                }
+
+            }
+        }
+    }
+}
